a7c74f10e1394df707bb740da4069f43033fb1ae
rutvikgondaliya <gondaliyarutvik281@gmail.com>
2020-12-18 16:08:42 +0530

    Add files via upload

---
+++ hackerrank_division.py
@@ -0,0 +1,25 @@
+a = int(input())
+b = int(input())
+
+print(a//b)
+print(a/b)
+
+"""
+   Python: Division - Hacker Rank Solution
+   Python: Division - Hacker Rank Solution
+    Task 
+    Read two integers and print two lines. The first line should contain integer division,  a//b . The second line should contain float division, a/b .
+
+    You don't need to perform any rounding or formatting operations.
+    Input Format 
+    The first line contains the first integer,a . The second line contains the second integer, b.
+    Output Format 
+    Print the two lines as described above.
+    Sample Input
+    4
+    3
+    sample Output
+    1
+    1.3333333333333333
+
+"""
